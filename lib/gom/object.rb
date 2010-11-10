37cb63ce13753b33a0d6c7eaadc4f43222d3cd20
Philipp Brüll <b.phifty@gmail.com>
2010-11-10 12:04:53 +0100

    introduced object proxy (addition)

--- lib/gom/object.rb
+++ lib/gom/object.rb
@@ -7,6 +7,7 @@ module GOM
     autoload :Injector, File.join(File.dirname(__FILE__), "object", "injector")
     autoload :Inspector, File.join(File.dirname(__FILE__), "object", "inspector")
     autoload :Mapping, File.join(File.dirname(__FILE__), "object", "mapping")
+    autoload :Proxy, File.join(File.dirname(__FILE__), "object", "proxy")
 
     def self.id(object)
       id = Mapping.id_by_object object
